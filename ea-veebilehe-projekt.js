08870ebb309bf8014941040cca9cd06b9d26fc17
Mia Grossthal <mia.grossthal@gmail.com>
2025-11-25 16:55:00 +0200

    javascripti kommentaarid

--- ea-veebilehe-projekt.js
+++ ea-veebilehe-projekt.js
@@ -1,17 +1,17 @@
-// Õiged vastused alamlehtedele
+// Pealehtede õiged vastused
 const oigedVastused = {
   "pealeht_1": "1011",
   "pealeht_2": "28",
   "pealeht_3": "5763",
 };
 
-// Lisa event listener kõikidele kontrolli nuppudele
+// Nupu kood
 document.querySelectorAll(".kontrolli").forEach(btn => {
   btn.addEventListener("click", () => {
     const alamleht = btn.dataset.leht;
     const vastusInput = document.querySelector(`#vastus[data-leht="${alamleht}"]`);
     const tulemusElem = document.querySelector(`#tulemus[data-leht="${alamleht}"]`);
-
+    // Output kui vastus on õige/vale
     if (vastusInput.value === oigedVastused[alamleht]) {
       tulemusElem.textContent = "Õige! ✅";
       tulemusElem.style.color = "green";
